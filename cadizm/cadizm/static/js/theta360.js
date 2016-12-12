0a820cd3e4913aa0b956fa894dd5fe6977139cff
Michael Cadiz <mcadiz@truecar.com>
2016-12-11 16:05:01 -0800

    self-hosted

--- cadizm/cadizm/static/js/theta360.js
+++ cadizm/cadizm/static/js/theta360.js
@@ -1,9 +1,1 @@
 
-function onVrViewLoad() {
-    var vrView = new VRView.Player('#vrview', {
-        image: '//static/images/360/1f1a15e6-bff8-11e6-a89c-34363bc43fea.jpg',
-        is_stereo: true
-    });
-}
-
-window.addEventListener('load', onVrViewLoad);
