6fd6ad9b1dc1af7bbde3c8b581c1c175108a28f4
Michael Cadiz <mcadiz@truecar.com>
2016-12-16 19:19:08 -0800

    initial working 360

---
+++ cadizm/cadizm/static/js/theta360.js
@@ -0,0 +1,16 @@
+
+function openOverlay(imgId) {
+    document.getElementById('overlay').style.width = '100%';
+
+    var width = $(document).width().toString();
+    var height = $(document).height().toString();
+
+    var iframe = $('#iframe')[0];
+    $(iframe).attr('src', ['https://theta360.com/s/', imgId, '?view=embed&width=', width, 'px&height=', height, 'px'].join(''));
+    $(iframe).attr('style', ['width:', width, 'px;height:', height, 'px; max-width: 100%;'].join(''));
+
+}
+
+function closeOverlay() {
+    document.getElementById('overlay').style.width = '0%';
+}
